a8104e835f21f788f3022336fdf35d0c8c6c2701
Yan_yao <2583614200@qq.com>
2025-10-19 17:02:51 +0800

    第一次测试用例达到预期失败

--- C++/cpp-training/include/Executor.hpp
+++ C++/cpp-training/include/Executor.hpp
@@ -2,24 +2,23 @@
 #include <string>
 namespace adas
 {
-    struct Pose
-    {
-        int x;
-        int y;
-        char heading;
-    };
+struct Pose {
+    int x;
+    int y;
+    char heading;
+};
 
-    class Executor
-    {
-    public:
-        static Executor *NewExecutor(const Pose &pose) noexcept;
-        Executor(void) = default;
-        virtual ~Executor(void) = default;
-        Executor(const Executor &) = delete;
-        Executor &operator=(const Executor &) = delete;
+class Executor
+{
+public:
+    static Executor* NewExecutor(const Pose& pose) noexcept;
+    Executor(void) = default;
+    virtual ~Executor(void) = default;
+    Executor(const Executor&) = delete;
+    Executor& operator=(const Executor&) = delete;
 
-    public:
-        virtual void Execute(const std::string &command) noexcept = 0;
-        virtual Pose Query(void) const noexcept = 0;
-    };
-} // namespace adas
+public:
+    virtual void Execute(const std::string& command) noexcept = 0;
+    virtual Pose Query(void) const noexcept = 0;
+};
+}  // namespace adas
