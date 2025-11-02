fa084a15da8d190a47be308c456887538c9fa64f
Yan_yao <2583614200@qq.com>
2025-11-02 16:07:23 +0800

    面向对象编程重构完成

--- C++/cpp-training/src/ExecutorImpl.cpp
+++ C++/cpp-training/src/ExecutorImpl.cpp
@@ -13,26 +13,26 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : pose(pose), isFast(false
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
     for (const auto cmd : commands) {
+        std::unique_ptr<ICommand> cmder;
+
         if (cmd == 'M') {
             if (isFast) {
-                std::unique_ptr<MoveCommand> cmder = std::make_unique<MoveCommand>();
-                cmder->DoOperate(*this);
+                cmder = std::make_unique<MoveCommand>();
             }
-            std::unique_ptr<MoveCommand> cmder = std::make_unique<MoveCommand>();
-            cmder->DoOperate(*this);
+            cmder = std::make_unique<MoveCommand>();
         } else if (cmd == 'L') {
             if (isFast) {
-                std::unique_ptr<MoveCommand> cmder = std::make_unique<MoveCommand>();
-                cmder->DoOperate(*this);
+                cmder = std::make_unique<MoveCommand>();
             }
-            std::unique_ptr<TurnLeftCommand> cmder = std::make_unique<TurnLeftCommand>();
-            cmder->DoOperate(*this);
+            cmder = std::make_unique<TurnLeftCommand>();
         } else if (cmd == 'R') {
             if (isFast) {
-                std::unique_ptr<MoveCommand> cmder = std::make_unique<MoveCommand>();
-                cmder->DoOperate(*this);
+                cmder = std::make_unique<MoveCommand>();
             }
-            std::unique_ptr<TurnRightCommand> cmder = std::make_unique<TurnRightCommand>();
+            cmder = std::make_unique<TurnRightCommand>();
+        }
+
+        if (cmder) {
             cmder->DoOperate(*this);
         }
     }
