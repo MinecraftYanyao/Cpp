304b23b78e60cb6d59a26d8fe264199506aecc90
Yan_yao <2583614200@qq.com>
2025-11-18 20:40:20 +0800

    表驱动提升扩展性

--- cpp-training/src/ExecutorImpl.cpp
+++ cpp-training/src/ExecutorImpl.cpp
@@ -1,6 +1,7 @@
 #include "ExecutorImpl.hpp"
 
 #include <memory>
+#include <unordered_map>
 
 #include "Command.hpp"
 
@@ -15,21 +16,16 @@ ExecutorImpl::ExecutorImpl(const Pose& pose) noexcept : poseHandler(pose)
 }
 void ExecutorImpl::Execute(const std::string& commands) noexcept
 {
+    std::unordered_map<char, std::unique_ptr<ICommand>> cmderMap;
+    cmderMap.emplace('M', std::make_unique<MoveCommand>());
+    cmderMap.emplace('L', std::make_unique<TurnLeftCommand>());
+    cmderMap.emplace('R', std::make_unique<TurnRightCommand>());
+    cmderMap.emplace('F', std::make_unique<FastCommand>());
     for (const auto cmd : commands) {
-        std::unique_ptr<ICommand> cmder;
+        const auto it = cmderMap.find(cmd);
 
-        if (cmd == 'M') {
-            cmder = std::make_unique<MoveCommand>();
-        } else if (cmd == 'L') {
-            cmder = std::make_unique<TurnLeftCommand>();
-        } else if (cmd == 'R') {
-            cmder = std::make_unique<TurnRightCommand>();
-        } else if (cmd == 'F') {
-            cmder = std::make_unique<FastCommand>();
-        }
-
-        if (cmder) {
-            cmder->DoOperate(poseHandler);
+        if (it != cmderMap.end()) {
+            it->second->DoOperate(poseHandler);
         }
     }
 }
